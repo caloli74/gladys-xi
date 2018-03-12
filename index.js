307ce149ab210ceac1f789ca44d1e38b32b424a9
unknown <caloli1974@gmail.com>
2018-03-12 19:57:16 +0100

    dev

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 module.exports = function (sails) {
 
     var install = require('./lib/install.js');
+    var work = require('./lib/work.js');
 
     return {
-        install: install
+        install: install,
+        work: work
     };
 };
