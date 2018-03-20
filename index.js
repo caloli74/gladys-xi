3dd9bbeb96e6a337608be125fd0290511a8e91ad
unknown <caloli1974@gmail.com>
2018-03-20 16:53:43 +0100

    dev

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 module.exports = function (sails) {
 
     var gateway = require('./lib/gateway.js');
+    var listen = require('./lib/listen.js');
     var install = require('./lib/install.js');
-    var init = require('./lib/init.js');
     var exec = require('./lib/exec.js');
+    var init = require('./lib/init.js');
     var work = require('./lib/work.js');
     var gateway2 = require('./lib/gateway2.js');
-    var listen = require('./lib/listen.js');
 
     gladys.on('ready', function(){
         listen();
@@ -15,8 +15,8 @@ module.exports = function (sails) {
     return {
         gateway: gateway,
         install: install,
-        init: init,
         exec: exec,
+        init: init,
         work: work,
         gateway2: gateway2
     };
